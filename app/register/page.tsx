3e360432ff7505f257144483fefec822746ea40a
BilalMP <peeroo270@gmail.com>
2025-03-12 13:47:34 +0400

    Register Page

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "@/components/RegisterForm";
+import React from "react";
+
+const RegisterPage = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <RegisterForm />
+        </div>
+    );
+};
+
+export default RegisterPage;

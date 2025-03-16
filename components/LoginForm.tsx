7f1c5aa8e521100d4771bab0f864f5ad363dfcbc
BilalMP <peeroo270@gmail.com>
2025-03-16 12:41:02 +0400

    Login Page

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,7 +1,143 @@
-import React from "react";
+"use client";
+
+import { loginSchema } from "@/schema";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTransition } from "react";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { toast } from "sonner";
 
 const LoginForm = () => {
-    return <div>LoginForm</div>;
+    const [isPending, startTransition] = useTransition();
+
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof loginSchema>) => {
+        startTransition(async () => {
+            try {
+                const response = await fetch("/api/login", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(values),
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    toast.success(data.message);
+                    form.reset();
+                    setTimeout(() => {
+                        window.location.href = "/";
+                    }, 300);
+                } else {
+                    const errorData = await response.json();
+                    // Handle specific error messages
+                    if (errorData.error) {
+                        toast.error(errorData.error);
+                    } else {
+                        toast.error("Sign In failed.");
+                    }
+                }
+            } catch (error) {
+                console.error("Sign In error:", error);
+                toast.error("An unexpected error occurred.");
+            }
+        });
+    };
+
+    return (
+        <Card className="w-[400px]">
+            <CardHeader>
+                <CardTitle>Sign In</CardTitle>
+                <CardDescription>Sign in to your account</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="example@example.com"
+                                            {...field}
+                                            type="email"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="*****"
+                                            {...field}
+                                            type="password"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button
+                            disabled={isPending}
+                            className="w-full"
+                            size="lg"
+                            type="submit"
+                        >
+                            Sign In
+                        </Button>
+                    </form>
+                </Form>
+            </CardContent>
+            <CardFooter className="flex justify-center items-center">
+                <Link
+                    href="/signup"
+                    className="text-xs hover:underline text-gray-600"
+                >
+                    Create an account
+                </Link>
+            </CardFooter>
+        </Card>
+    );
 };
 
 export default LoginForm;

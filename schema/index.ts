78cfd0a79b7cd337039254fa528a77a076402ddf
BilalMP <peeroo270@gmail.com>
2025-03-30 13:44:18 +0400

    Schema

--- schema/index.ts
+++ schema/index.ts
@@ -51,3 +51,24 @@ export const loginSchema = z.object({
         message: "Password must be at least 6 characters",
     })
 })
+
+export const blogSchema = z.object({
+    title: z.string().min(1).max(100, {
+        message: "Title must be between 1 and 100 characters",
+    }),
+    excerpt: z.string().min(1).max(500, { 
+        message: "Excerpt must be between 1 and 500 characters",
+    }),
+    content: z.string().min(1, {
+        message: "Content is required",
+    }),
+    coverImage: z.string().min(1, {
+        message: "Cover image is required",
+    }),
+    readingTime: z.string().min(1, {
+        message: "Reading time is required",
+    }),
+    categories: z.string().min(1, {
+        message: "At least one category is required",
+    }),
+})

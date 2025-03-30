d5c4866bb70a5699bebcc34a3d717d7eaff355e8
BilalMP <peeroo270@gmail.com>
2025-03-30 13:44:03 +0400

    Account

--- components/Account.tsx
+++ components/Account.tsx
@@ -18,17 +18,35 @@ const Account = () => {
                 <DropdownMenuTrigger>
                     {session?.user
                         ? `Hello, ${
-                                session.user.name ||
-                                session.user.email?.split("@")[0] ||
-                                "User"
-                            }`
+                              session.user.name ||
+                              session.user.email?.split("@")[0] ||
+                              "User"
+                          }`
                         : "Account"}
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
                     {session?.user ? (
-                        <DropdownMenuItem onClick={() => signOut()}>
-                            Logout
-                        </DropdownMenuItem>
+                        <>
+                            <DropdownMenuItem>
+                                <Link
+                                    href={"/dashboard"}
+                                    className="text-sm font-medium hover:opacity-70 transition-opacity relative py-1 text-foreground after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-foreground"
+                                >
+                                    Dashboard
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link
+                                    href={"/add-blog"}
+                                    className="text-sm font-medium hover:opacity-70 transition-opacity relative py-1 text-foreground after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-foreground"
+                                >
+                                    Add Blog
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => signOut()}>
+                                Logout
+                            </DropdownMenuItem>
+                        </>
                     ) : (
                         <>
                             <DropdownMenuItem>
